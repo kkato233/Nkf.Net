b7a395f9286d2bd859fd7ae41656c4f83594503d
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2019-10-22 10:34:51 +0900

    クリーン環境でビルドできなかったので build.bat 修正 Visual Studio 2019 の .vs フォルダを .gitignore 追加

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
-rem msbuild Nkf.Net-2.0.sln /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v2.0
+cd proj_std2.0
+dotnet build
+cd ..
 msbuild proj_4.0\Nkf.Net-4.0.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=v4.0
-msbuild proj_4.5\Nkf.Net-4.5.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=v4.5
-msbuild proj_4.5.1\Nkf.Net-4.5.1.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=v4.5.1
 msbuild proj_std2.0\Nkf.Net.Core.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=2.0
-msbuild proj_std2.0\Nkf.Net.Core.sln /t:pack /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:Configuration=Release;TargetFrameworkVersion=2.0
 
 nuget pack Nkf.Net.nuspec
