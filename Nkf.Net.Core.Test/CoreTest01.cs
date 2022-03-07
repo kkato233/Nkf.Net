f3a85038ea73a8edf43f405436d6b39cd60bff7f
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2022-03-07 23:36:14 +0900

    GetVersion テスト追加

---
+++ Nkf.Net.Core.Test/CoreTest01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Nkf.Net.Core.Test
+{
+    [TestClass]
+    public class CoreTest01
+    {
+        [TestMethod]
+        public void GetVersion()
+        {
+            string ver = Nkf.Net.WrapNkf.GetNkfVersion();
+            Console.WriteLine($"ver {ver}");
+        }
+    }
+}
