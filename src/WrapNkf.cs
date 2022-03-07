95b6ab7791f605c8a7c03fe465245dc7a1f0f310
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2022-03-08 01:34:48 +0900

    GetGuess 実装変更

--- src/WrapNkf.cs
+++ src/WrapNkf.cs
@@ -222,7 +222,7 @@ namespace Nkf.Net
             NativeMethods.GetNkfGuess(sb, sb.Capacity, out len);
             len = Math.Min(len, sb.Length);
 
-            return sb.ToString(0, len);
+            return sb.ToString();
         }
 
         public static string GetSupportFunctions()
