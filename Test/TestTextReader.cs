1e1e3ec2078ee483714dee2b292b64d958360d00
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2023-07-02 12:57:47 +0900

    改行コード関連のテストコード・テストファイル追加

--- Test/TestTextReader.cs
+++ Test/TestTextReader.cs
@@ -65,6 +65,8 @@ namespace Nkf.Net.Test
                     Assert.AreEqual("EUC-JP - ウィキペディア - Wikipedia", s2);
                     Assert.AreEqual("CP51932はマイクロソフトがWindowsで使用しているWindows-31JのEUC-JP互換表現。実装例はInternet Explorer4.0以降、EmEditor、秀丸エディタ等。このコードはNECのPC-9800シリーズの漢字コード（9区から12区の特殊文字を除外したもの）", s3);
                     Assert.IsNull(s4);
+
+                    Assert.AreEqual("\r\n", sr.LastEOL); // 改行コード判定
                 }
             }
         }
@@ -72,7 +74,6 @@ namespace Nkf.Net.Test
         [TestMethod]
         public void TestNKFFileRead3()
         {
-            // 文字列が途中で泣き別れている部分の動作確認
             List<string> files = new List<string>()
             {
                 "sjis-big.txt"
@@ -108,6 +109,83 @@ namespace Nkf.Net.Test
         }
 
         [TestMethod]
+        public void TestNKFFileRead_LF()
+        {
+            List<string> files = new List<string>()
+            {
+                "sjis-LF.txt"
+            };
+
+            foreach (string file in files)
+            {
+                string fileName = "TestData" + System.IO.Path.DirectorySeparatorChar + file;
+                System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(System.Environment.CurrentDirectory);
+                if (System.IO.File.Exists(fileName) == false)
+                {
+                    dir = dir.Parent.Parent;
+                    dir = new System.IO.DirectoryInfo(System.IO.Path.Combine(dir.FullName, "TestData"));
+
+                    fileName = System.IO.Path.Combine(dir.FullName, file);
+                }
+                using (var sr = new NkfTextReader(fileName))
+                {
+                    for (; ; )
+                    {
+                        string s1 = sr.ReadLine();
+                        string s2 = sr.ReadLine();
+                        string s3 = sr.ReadLine();
+
+                        if (s1 == null) break;
+
+                        Assert.AreEqual("「EUC」とは - End User Computingの略。 ベンダーにより提供されたアプリケーション以外に、目的に応じてエンドユーザーが独自にアプリケーションを作成すること。 ベンダーはEUCができるようにするための基本的な...", s1);
+                        Assert.AreEqual("EUC-JP - ウィキペディア - Wikipedia", s2);
+                        Assert.AreEqual("CP51932はマイクロソフトがWindowsで使用しているWindows-31JのEUC-JP互換表現。実装例はInternet Explorer4.0以降、EmEditor、秀丸エディタ等。このコードはNECのPC-9800シリーズの漢字コード（9区から12区の特殊文字を除外したもの）", s3);
+
+                        Assert.AreEqual("\n", sr.LastEOL); // 改行コード判定
+                    }
+                }
+            }
+        }
+        [TestMethod]
+        public void TestNKFFileRead_CR()
+        {
+            List<string> files = new List<string>()
+            {
+                "sjis-CR.txt"
+            };
+
+            foreach (string file in files)
+            {
+                string fileName = "TestData" + System.IO.Path.DirectorySeparatorChar + file;
+                System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(System.Environment.CurrentDirectory);
+                if (System.IO.File.Exists(fileName) == false)
+                {
+                    dir = dir.Parent.Parent;
+                    dir = new System.IO.DirectoryInfo(System.IO.Path.Combine(dir.FullName, "TestData"));
+
+                    fileName = System.IO.Path.Combine(dir.FullName, file);
+                }
+                using (var sr = new NkfTextReader(fileName))
+                {
+                    for (; ; )
+                    {
+                        string s1 = sr.ReadLine();
+                        string s2 = sr.ReadLine();
+                        string s3 = sr.ReadLine();
+
+                        if (s1 == null) break;
+
+                        Assert.AreEqual("「EUC」とは - End User Computingの略。 ベンダーにより提供されたアプリケーション以外に、目的に応じてエンドユーザーが独自にアプリケーションを作成すること。 ベンダーはEUCができるようにするための基本的な...", s1);
+                        Assert.AreEqual("EUC-JP - ウィキペディア - Wikipedia", s2);
+                        Assert.AreEqual("CP51932はマイクロソフトがWindowsで使用しているWindows-31JのEUC-JP互換表現。実装例はInternet Explorer4.0以降、EmEditor、秀丸エディタ等。このコードはNECのPC-9800シリーズの漢字コード（9区から12区の特殊文字を除外したもの）", s3);
+
+                        Assert.AreEqual("\r", sr.LastEOL); // 改行コード判定
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
         public void TestMiniText()
         {
 
@@ -134,6 +212,7 @@ namespace Nkf.Net.Test
                     Assert.IsNotNull(s);
                     Assert.IsTrue(s.Length > 1);
                     Console.WriteLine(s);
+                    Assert.AreEqual("", sr.LastEOL); // 改行コード判定
 
                 }
             }
