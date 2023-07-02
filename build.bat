099b857c6a4aea79fff846a023f44c319bfe69cd
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2023-07-02 13:57:19 +0900

    nuget パッケージバージョン 1.0.0 対応

--- build.bat
+++ build.bat
@@ -1,7 +1,10 @@
 cd proj_std2.0
 dotnet build
 cd ..
-msbuild proj_4.0\Nkf.Net-4.0.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=v4.0
-msbuild proj_std2.0\Nkf.Net.Core.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=2.0
+
+msbuild proj_4.0\Nkf.Net-4.0.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=v4.8 /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg
+msbuild proj_std2.0\Nkf.Net.Core.sln /t:Rebuild  /p:Configuration=Release;TargetFrameworkVersion=2.0 /t:pack /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg
 
 nuget pack Nkf.Net.nuspec
+nuget pack Nkf.Net.nuspec -Symbols -SymbolPackageFormat snupkg
+
